a3ced3110e7ce30b45976a9ca87610dd51925498
rathapong <rathapong@promptnow.com>
2024-06-01 12:22:03 +0700

    test hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './App.css';
+import LiffComponent from './components/LiffComponent';
 
 function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
+        <LiffComponent />
+      </header>
     </div>
   );
 }
---
+++ src/components/LiffComponent.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import liff from '@line/liff';
+
+const LiffComponent = () => {
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    const initializeLiff = async () => {
+      try {
+        await liff.init({ liffId: '2005387393-XvmK0M34' });
+        if (!liff.isLoggedIn()) {
+          liff.login();
+        } else {
+          const userProfile = await liff.getProfile();
+          setProfile(userProfile);
+          console.log('User Profile:', userProfile);
+
+          const accessToken = liff.getAccessToken();
+          console.log('Access Token:', accessToken);
+
+          sendNotification(userProfile.userId);
+        }
+      } catch (error) {
+        console.error('LIFF Initialization failed', error);
+      }
+    };
+
+    const sendNotification = async (userId) => {
+        try {
+          await fetch('https://8f31-2001-44c8-4280-dbc4-70cb-d753-a4d6-cf78.ngrok-free.app/webhook', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              userId: userId,
+              message: 'You have successfully logged in!',
+            }),
+          });
+        } catch (error) {
+          console.error('Error sending notification', error);
+        }
+      };
+
+    initializeLiff();
+  }, []);
+
+  return (
+    <div>
+      <h1>LIFF App</h1>
+      {profile ? (
+        <div>
+          <p>Name: {profile.displayName}</p>
+          <p>User ID: {profile.userId}</p>
+          <p>Status Message: {profile.statusMessage}</p>
+          <img src={profile.pictureUrl} alt="Profile" />
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default LiffComponent;
