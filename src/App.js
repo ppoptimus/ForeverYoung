b11b93e32778191d61573b78da555d6be7778d7d
rathapong <rathapong@promptnow.com>
2024-06-19 12:31:23 +0700

    recode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import liff from "@line/liff";
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
   const [profile, setProfile] = useState(null);
+
   useEffect(() => {
     liff
       .init({ liffId: "2005387393-XvmK0M34" })
@@ -20,6 +21,18 @@ function App() {
       .catch((err) => console.error("LIFF Initialization failed:", err));
   }, []);
 
+  const handleScanQRCode = () => {
+    try {
+      liff.scanCodeV2().then((result) => {
+        console.log('Scan result:', result.value);
+        // เพิ่มโค้ดเพื่อใช้งานผลลัพธ์ที่ได้จากการสแกน QR code ตามต้องการ
+      });
+    } catch (error) {
+      console.error('Error scanning QR code:', error);
+      // จัดการข้อผิดพลาดที่เกิดขึ้น
+    }
+  };
+
   return (
     <div>
       <h1>QR Code Scanner</h1>
@@ -31,12 +44,12 @@ function App() {
       )}
       <div>
         <button
-          onClick={QrScannerComponent()}
+          onClick={handleScanQRCode}
           type="button"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           Scan Point
           <svg
-            class="ml-2 w-6 h-6 text-white dark:text-white"
+            className="ml-2 w-6 h-6 text-white dark:text-white"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -45,14 +58,14 @@ function App() {
             viewBox="0 0 24 24">
             <path
               stroke="currentColor"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M4 4h6v6H4V4Zm10 10h6v6h-6v-6Zm0-10h6v6h-6V4Zm-4 10h.01v.01H10V14Zm0 4h.01v.01H10V18Zm-3 2h.01v.01H7V20Zm0-4h.01v.01H7V16Zm-3 2h.01v.01H4V18Zm0-4h.01v.01H4V14Z"
             />
             <path
               stroke="currentColor"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M7 7h.01v.01H7V7Zm10 10h.01v.01H17V17Z"
             />
           </svg>
@@ -61,28 +74,5 @@ function App() {
     </div>
   );
 }
-
-const QrScannerComponent = () => {
-  const [data, setData] = useState("No result");
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    try {
-      liff.scanCodeV2().then((result) => {
-        setData(result.value);
-      });
-    } catch (error) {
-      setError(error);
-    }
-  }, []);
-
-  return (
-    <div>
-      <h1>QR Code Scanner</h1>
-      {error ? <p>{error}</p> : <p>{data}</p>}
-      <p>{data}</p>
-    </div>
-  );
-};
 
 export default App;
